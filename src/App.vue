76398dd82411ba3c00b76579df59c593e041ffdf
AgustinTroncoso <a.troncoso09@ufromail.cl>
2024-05-17 16:05:59 -0400

    c
    c

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Padre from "./components/Padre.vue";
+import Hijo from "./components/Hijo.vue";
+</script>
+
+<template>
+  <div id="app">
+    <Padre />
+    <Hijo />
+  </div>
+</template>
